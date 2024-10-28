1441e214a90eeb5c20f1fb1a619a1cfa2781fb69
Tajio68 <=>
2024-10-28 09:41:07 +0100

    Mark Status

--- functions/mark_done.py
+++ functions/mark_done.py
@@ -1,2 +1,21 @@
-def mark_done():
-  print('markdone')
+import json
+from datetime import datetime
+
+def mark_done(id):
+  
+  # OPEN FILE
+  
+  with open('data.json') as f:
+    data = json.load(f)
+
+  # FIND OBJECT
+    
+  for item in data:
+    if item['id'] == id:
+      item['status'] = 'done'
+      item['updatedAt'] = str(datetime.now())[:19]
+  
+  # WRITE DATA
+  
+  with open('data.json', 'w') as f:
+    json.dump(data, f, indent=4)
--- functions/mark_in_progress.py
+++ functions/mark_in_progress.py
@@ -1,2 +1,21 @@
-def mark_in_progress():
-  print('markinprogress')
+import json
+from datetime import datetime
+
+def mark_in_progress(id):
+  
+  # OPEN FILE
+  
+  with open('data.json') as f:
+    data = json.load(f)
+
+  # FIND OBJECT
+    
+  for item in data:
+    if item['id'] == id:
+      item['status'] = 'in-progress'
+      item['updatedAt'] = str(datetime.now())[:19]
+  
+  # WRITE DATA
+  
+  with open('data.json', 'w') as f:
+    json.dump(data, f, indent=4)
