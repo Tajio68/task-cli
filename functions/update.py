551c5ead85fc95be41ca50c2efcfe6b36730ff8f
Tajio68 <=>
2024-10-28 08:45:02 +0100

    Update

--- functions/update.py
+++ functions/update.py
@@ -1,2 +1,22 @@
-def update():
-  print('update')
+import json
+from datetime import datetime
+
+def update(id, name):
+  
+  # OPEN FILE
+  
+  with open('data.json') as f:
+    data = json.load(f)
+    
+  # SEARCH OF THE ID
+    
+  for item in data:
+    if item['id'] == id:
+      item['desc'] = name
+      item['updatedAt'] = str(datetime.now())[:19]
+      break
+    
+  # WRITING THE NEW DATA
+    
+  with open('data.json', 'w') as f:
+    json.dump(data, f, indent=4)
