c4d5a831fbf8aa7e2d180dc465f1da99d36244f8
Tajio68 <=>
2024-10-28 09:27:34 +0100

    Delete

--- functions/delete.py
+++ functions/delete.py
@@ -1,2 +1,17 @@
-def delete():
-  print('delete')
+import json
+
+def delete(id):
+  
+  # OPEN FILE
+  
+  with open('data.json') as f:
+    data = json.load(f)
+    
+  # EXCLUDE TARGET OBJECT FROM DATA
+    
+  data = [item for item in data if item['id'] != id]
+  
+  # WRITE JSON
+
+  with open('data.json', 'w') as f:
+    json.dump(data, f, indent=4)
