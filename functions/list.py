11b03884f9b8c163bc024ed3f6db9f093c397f53
Tajio68 <=>
2024-10-28 10:16:37 +0100

    List

--- functions/list.py
+++ functions/list.py
@@ -1,2 +1,16 @@
-def listTask():
-  print('list')
+import json
+
+def listTask(status):
+  
+  # OPEN FILE
+  
+  with open('data.json') as f:
+    data = json.load(f)
+  
+  # DISPLAY TASKS
+  
+  for item in data:
+    if item['status'] == status:
+      print(f'({item["id"]}) {item["desc"]} : {item["status"]}')
+    elif status == None:
+      print(f'({item["id"]}) {item["desc"]} : {item["status"]}')
